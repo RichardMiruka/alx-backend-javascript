d95e9595c13a0239445162b003d62f14c039c85e
RichardMiruka <richardonsare15@gmail.com>
2024-03-05 12:12:13 +0300

    removed the folder  to start afresh

--- example_exercises/binding.js
+++
@@ -1,8 +0,0 @@
-var John = {
-    name: 'John',
-    greet: function(person) {
-        alert("Hi " + pesron + ", my name is " + this.name);
-    }
-};var fx = John.greet;
-
-fx('Paul'); // Hi Paul, my name is undefined 
