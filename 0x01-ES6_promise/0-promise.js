eb7757259faa4e901c9772b4d62be9d196af99cd
RichardMiruka <richardonsare15@gmail.com>
2024-03-08 13:48:01 +0300

    fixing an error

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,3 +1,9 @@
 export default function iterateThroughObject(reportWithIterator) {
-  return reportWithIterator.join(' | ');
+  if (reportWithIterator && Array.isArray(reportWithIterator)) {
+    return reportWithIterator.join(' | ');
+  }
+  // Handle the case when reportWithIterator is not an array or is undefined
+  console.error('Invalid input for iterateThroughObject:', reportWithIterator);
+  // You may choose to return a default value or throw an error here
+  // Example: return 'Default Value';
 }
