1fdeb1fb9289da54627210d686dc24c3f85fbda0
RichardMiruka <richardonsare15@gmail.com>
2024-03-08 14:35:03 +0300

    handle multiple promises

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -2,8 +2,18 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
-  return signUpUser(firstName, lastName)
-    .then((value) => uploadPhoto(fileName))
-    .then((value) => console.log(`${value.body} ${value.firstName} ${value.lastName}`))
-    .catch(() => console.log('Signup system offline'));
+  const signUpUserPromise = signUpUser(firstName, lastName);
+  const uploadPhotoPromise = uploadPhoto(fileName);
+
+  return Promise.allSettled([signUpUserPromise, uploadPhotoPromise]).then((values) => {
+    const result = [];
+    values.forEach((element) => {
+      if (element.status === 'fulfilled') {
+        result.push({ status: element.status, value: element.value });
+      } else {
+        result.push({ status: element.status, value: `${element.reason}` });
+      }
+    });
+    return result;
+  });
 }
