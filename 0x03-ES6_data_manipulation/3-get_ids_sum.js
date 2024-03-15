a8d59cf36c18717377093a2d68bb1461396814d5
RichardMiruka <richardonsare15@gmail.com>
2024-03-15 12:43:52 +0300

    reduce function on the array

---
+++ 0x03-ES6_data_manipulation/3-get_ids_sum.js
@@ -0,0 +1,8 @@
+const getStudentIdsSum = (students) => {
+  if (!Array.isArray(students)) {
+    return 0;
+  }
+  return students.reduce((acc, student) => acc + student.id, 0);
+};
+
+export default getStudentIdsSum;
