aea9dd5b4bd8b0f8132c474ee26ed20e541861d1
RichardMiruka <richardonsare15@gmail.com>
2024-04-29 21:01:44 +0300

    made comments on a common example Hello World of Node.js - a web server

--- 0x05-Node_JS_basic/server.js
+++ 0x05-Node_JS_basic/server.js
@@ -1,12 +1,12 @@
 const { createServer } = require('node:http'); // Import the http module
 
-const hostname = '127.0.0.1';
+const hostname = '127.0.0.1'; // Defines the hostname as localhost - server is set to listen on the specified port on the local machine
 const port = 3000;
 
-const server = createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello World');
+const server = createServer((req, res) => { // Creates a new HTTP server object and returns it
+    res.statusCode = 200; // Sets the status code of the response to 200 OK / successful
+    res.setHeader('Content-Type', 'text/plain'); // Sets the content type of the response to plain text
+    res.end('Hello World'); // Sends a response with the message "Hello World"
 });
 
 server.listen(port, hostname, () => {
