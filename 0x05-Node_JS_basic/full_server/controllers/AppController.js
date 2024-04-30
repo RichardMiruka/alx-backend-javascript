dacfac1fb35093b0a3d091cd442f38bfd8787eb6
RichardMiruka <richardonsare15@gmail.com>
2024-04-30 19:40:28 +0300

    Utility functions

--- 0x05-Node_JS_basic/full_server/controllers/AppController.js
+++ 0x05-Node_JS_basic/full_server/controllers/AppController.js
@@ -1,6 +1,6 @@
 class AppController {
-    static getHomePage(request, response) {
-        response.status(200).send('Hello Holberton School!');
-    }   
-    }
-    module.exports = AppController;
+  static getHomePage(request, response) {
+    response.status(200).send('Hello Holberton School!');
+  }
+}
+module.exports = AppController;
