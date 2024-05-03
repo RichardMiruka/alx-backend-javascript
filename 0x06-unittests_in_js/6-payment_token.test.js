b3f01cc82fa647f14c6d2036060f82257844b004
RichardMiruka <richardonsare15@gmail.com>
2024-05-04 01:29:48 +0300

    Async tests with done

--- 0x06-unittests_in_js/6-payment_token.test.js
+++ 0x06-unittests_in_js/6-payment_token.test.js
@@ -1,13 +1,12 @@
-const { describe, it } = require('mocha');
 const { expect } = require('chai');
 const getPaymentTokenFromAPI = require('./6-payment_token');
 
 describe('getPaymentTokenFromAPI', () => {
-  it('async testing with done callback', () => new Promise((done) => {
+  it('getPaymentTokenFromAPI(success), where success == true', (done) => {
     getPaymentTokenFromAPI(true)
-	    .then((data) => {
-        expect(data).to.have.property('data');
+      .then((res) => {
+        expect(res).to.deep.equal({data: 'Successful response from the API'});
         done();
-	    });
-  }));
+      });
+  });
 });
