8924c414e8ea672b465806323fdfb3e3afe3fed1
RichardMiruka <richardonsare15@gmail.com>
2024-05-03 22:39:07 +0300

    Basic test with Mocha and Node assertion library

--- 0x06-unittests_in_js/0-calcul.js
+++ 0x06-unittests_in_js/0-calcul.js
@@ -1,3 +1,6 @@
+// Create a function named calculateNumber.
+// It should accepts two arguments (number) a and b
+// The function should round a and b and return the sum of it
 function calculateNumber(a, b) {
   const an = Math.round(a);
   const bn = Math.round(b);
