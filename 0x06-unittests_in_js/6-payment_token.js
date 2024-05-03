3a13016ec6f0653de0758713bd90e591d1db7aca
RichardMiruka <richardonsare15@gmail.com>
2024-05-04 01:26:05 +0300

    how to support async testing, for example when waiting for the answer of an API or from a Promise

--- 0x06-unittests_in_js/6-payment_token.js
+++ 0x06-unittests_in_js/6-payment_token.js
@@ -1,9 +1,7 @@
-function getPaymentTokenFromAPI(success) {
+const getPaymentTokenFromAPI = (success) => new Promise((resolve, _reject) => {
   if (success) {
-    return new Promise((resolve) => {
-      resolve({ data: 'Successful response from the API' });
-    });
+    resolve({data: 'Successful response from the API'});
   }
-}
+});
 
 module.exports = getPaymentTokenFromAPI;
